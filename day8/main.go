a3a07ad093d13ee8c4be8826bfe409b3e66e46f7
Viktor <>
2022-12-14 14:11:13 +0100

    add part two

---
+++ day8/main.go
@@ -0,0 +1,59 @@
+package maind
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("file.txt")
+	if err != nil {
+		return
+	}
+	reader := bufio.NewReader(file)
+	treeMap := make([][]int8, 99)
+	for i, _ := range treeMap {
+		var lineBytes []byte
+		lineBytes, _, err = reader.ReadLine()
+		if err != nil {
+			break
+		}
+		line := string(lineBytes)
+		treeMap[i] = make([]int8, len(line))
+		for c, v := range line {
+			treeMap[i][c] = int8(v - '0')
+		}
+	}
+	count := 4*99 - 4
+	for y := 0; y < len(treeMap); y++ {
+		for x := 0; x < len(treeMap[0]); x++ {
+			var maxLeft int8 = 0
+			var maxRight int8 = 0
+			for sX := 0; sX < len(treeMap[0]); sX++ {
+				if sX < x && maxLeft < treeMap[y][sX] {
+					maxLeft = treeMap[y][sX]
+				} else if sX > x && maxRight < treeMap[y][sX] {
+					maxRight = treeMap[y][sX]
+				}
+			}
+
+			var maxTop int8 = 0
+			var maxDown int8 = 0
+			for sY := 0; sY < len(treeMap); sY++ {
+				if sY < y && maxTop < treeMap[sY][x] {
+					maxTop = treeMap[sY][x]
+				} else if sY > y && maxDown < treeMap[sY][x] {
+					maxDown = treeMap[sY][x]
+				}
+			}
+			if x == 0 || x == 98 || y == 0 || y == 98 {
+				continue
+			}
+			if maxLeft < treeMap[y][x] || maxRight < treeMap[y][x] || maxDown < treeMap[y][x] || maxTop < treeMap[y][x] {
+				count++
+			}
+		}
+	}
+	fmt.Println(count)
+}
---
+++ day8/mainPartTwo.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("file.txt")
+	if err != nil {
+		return
+	}
+	reader := bufio.NewReader(file)
+	treeMap := make([][]int8, 99)
+	for i, _ := range treeMap {
+		var lineBytes []byte
+		lineBytes, _, err = reader.ReadLine()
+		if err != nil {
+			break
+		}
+		line := string(lineBytes)
+		treeMap[i] = make([]int8, len(line))
+		for c, v := range line {
+			treeMap[i][c] = int8(v - '0')
+		}
+	}
+	bestScenic := 0
+	for y := 0; y < len(treeMap); y++ {
+		for x := 0; x < len(treeMap[0]); x++ {
+			var distLeft int = 0
+			var distRight int = 0
+			var distTop int = 0
+			var distBottom int = 0
+
+			for sX := x - 1; sX >= 0; sX-- {
+				distLeft++
+				if treeMap[y][sX] >= treeMap[y][x] {
+					break
+				}
+			}
+			for sX := x + 1; sX < 99; sX++ {
+				distRight++
+				if treeMap[y][sX] >= treeMap[y][x] {
+					break
+				}
+			}
+
+			for sY := y - 1; sY >= 0; sY-- {
+				distTop++
+				if treeMap[sY][x] >= treeMap[y][x] {
+					break
+				}
+
+			}
+
+			for sY := y + 1; sY < 99; sY++ {
+				distBottom++
+				if treeMap[sY][x] >= treeMap[y][x] {
+					break
+				}
+			}
+
+			scenic := distBottom * distTop * distLeft * distRight
+			if scenic > bestScenic {
+				bestScenic = scenic
+			}
+		}
+	}
+	fmt.Println(bestScenic)
+}
